957981afa96b8278703f0f53c80a416e19f294da
Autumn Fjeld <autumn@ziploop.com>
2016-02-07 18:57:55 -0800

    Move twitter route code to own file

--- api.js
+++ api.js
@@ -1,25 +1,10 @@
 var express = require('express');
+
 var app = express();
 
-app.get('/api/mocktweets', function(req,res){
-  res.json([
-    {
-      id_str: "240859602684612608",
-      user: {screen_name: 'henri'},
-      text: 'node rocks!'
-    },
-    {
-      id_str: "240859602684612699",
-      user: {screen_name: 'henri'},
-      text: 'yea. a day of sunshine!'
-    },
-    {
-      id_str: "240859602684612700",
-      user: {screen_name: 'henri'},
-      text: 'rain rain go away, come again another day!'
-    },    
-  ])
-});
+var twitter = require('./twitter_route.js');
+
+app.get('/api/mocktweets', twitter.mockTweets);
 
 app.listen(3000, function () {
   console.log('Server listening on', 3000)
---
+++ twitter_route.js
@@ -0,0 +1,21 @@
+exports.mockTweets = mockTweets;
+
+function mockTweets(req, res){
+  res.json([
+    {
+      id_str: "240859602684612608",
+      user: {screen_name: 'henri'},
+      text: 'node rocks!'
+    },
+    {
+      id_str: "240859602684612699",
+      user: {screen_name: 'henri'},
+      text: 'yea. a day of sunshine!'
+    },
+    {
+      id_str: "240859602684612700",
+      user: {screen_name: 'henri'},
+      text: 'rain rain go away, come again another day!'
+    },    
+  ])  
+}
