f578ff4273b4fc6d66300fca070dad196cc0224a
Autumn Fjeld <autumn@ziploop.com>
2016-02-07 19:09:20 -0800

    Add node-twitter-api module, twitter auth data, confirm successful connection to twitter api

--- twitter_route.js
+++ twitter_route.js
@@ -1,4 +1,26 @@
+var twitterAPI = require('node-twitter-api');
+
 exports.mockTweets = mockTweets;
+
+var twitter = new twitterAPI({
+  consumerKey: "ATwRHfjXNiI3dSjzAsgFxATM3",
+  consumerSecret: "tz1UAZwf754Uovs2Up2lco61etgtheZ3xzCtTeGQhh59zEJ5FH"
+  // callback: 'http://something'
+});
+
+//My own twitter access data for testing
+var config = {
+  accessToken: "27513075-ObsIk211WdFcMAiRiLF1gyUSmexKvcgSFGwuuKIYH",
+  accessTokenSecret: "DaB7A57v0UtmgVariGvzVFKeGyVE5SNVeXca9awVMGHwC",  
+};
+
+twitter.verifyCredentials(config.accessToken, config.accessTokenSecret, function(error, data, response) {
+  if (error) {
+    console.log('ERROR: verifyCredentials', error);
+  } else {
+    console.log(data["screen_name"], data);
+  }
+});
 
 function mockTweets(req, res){
   res.json([
