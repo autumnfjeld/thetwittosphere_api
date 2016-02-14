0dd64b00a803142c2c2684b3255aaf6441f8f403
Autumn Fjeld <autumn@ziploop.com>
2016-02-14 11:45:24 -0800

    Enable Cross Origin Resource Sharing in express

--- api.js
+++ api.js
@@ -1,13 +1,26 @@
-var express = require('express');
+var express = require('express'),
+    twitter = require('./twitter_route.js');
 
 var app = express();
 
-var twitter = require('./twitter_route.js');
+var allowCrossDomain = function(req, res, next) {
+  //simple CORS handling
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Authorization, Content-Length, X-Requested-With');
+  
+  // intercept OPTIONS method
+  (req.method !== 'OPTIONS') ? next() : res.send(200);
+};
+//consider https://www.npmjs.com/package/cors
+
+app.use(allowCrossDomain);
 
 app.get('/api/tweetsbyuser', twitter.getTweetsByUser);
 app.post('/api/posttweet', twitter.update);
 app.get('/api/mocktweets', twitter.mockTweets);
 
 app.listen(3000, function () {
-  console.log('Server listening on', 3000)
+  console.log('Server listening on', 3000)   //check on localhost:3000/api/routexxx
 });
